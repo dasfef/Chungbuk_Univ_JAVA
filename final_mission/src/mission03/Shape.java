1575145d17980c6cd484a2226d93bb650413d067
dasfef <96065385+dasfef@users.noreply.github.com>
2023-04-25 17:36:17 +0900

    Add files via upload

--- final_mission/src/mission03/Shape.java
+++ final_mission/src/mission03/Shape.java
@@ -4,13 +4,7 @@ public abstract class Shape {
 	double area;
 	String color;
 	String type;
-	
-	public Shape(double area, String color, String type) {
-		this.area = area;
-		this.color = color;
-		this.type = type;
-	}
-	
+		
 	public abstract void draw();
 	public abstract void setData();
 	public abstract double getArea();
